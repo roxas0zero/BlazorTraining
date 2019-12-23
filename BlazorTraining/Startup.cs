28cbc34d105d83645a2f520f081db44283feae98
Abdullah Hashim <roxas0zero@gmail.com>
2019-12-23 19:36:55 +0300

    parent and child components data binding

--- BlazorTraining/Startup.cs
+++ BlazorTraining/Startup.cs
@@ -25,8 +25,8 @@ namespace BlazorTraining
         // This method gets called by the runtime. Use this method to add services to the container.
         // For more information on how to configure your application, visit https://go.microsoft.com/fwlink/?LinkID=398940
         public void ConfigureServices(IServiceCollection services)
-        {
-            services.AddRazorPages();
+        {            
+            services.AddRazorPages().AddRazorRuntimeCompilation();
             services.AddServerSideBlazor();
             services.AddSingleton<WeatherForecastService>();
         }
