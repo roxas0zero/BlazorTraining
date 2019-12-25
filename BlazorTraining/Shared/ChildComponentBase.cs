4414426696468d8fe1655527f8791ff1af3185eb
Abdullah Hashim <roxas0zero@gmail.com>
2019-12-25 22:04:01 +0300

    component base class

---
+++ BlazorTraining/Shared/ChildComponentBase.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Components;
+
+namespace BlazorTraining.Shared
+{
+    public class ChildComponentBase : ComponentBase
+    {
+        protected bool DarkThemeOn;
+        protected string AlertTheme => DarkThemeOn ? "dark" : "light";
+
+        [Parameter]
+        public RenderFragment ChildContent { get; set; }
+
+        protected override void OnInitialized()
+        {
+            DarkThemeOn = true;
+        }
+    }
+}
